249c0e37be39d4cc13ed10ab1e9138f5bcbcb5bf
lion0024 <lion0024.xyz@gmail.com>
2020-03-20 16:32:13 +0900

    initial commit

---
+++ teltelnet.c
@@ -0,0 +1,100 @@
+/* rls.c -	リモートディレクトリ表示サービスのクライアント
+ * 					使い方：rls <ホスト名> <ディレクトリ>
+ */
+
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define oops(msg)	{ perror(msg); exit(1); }
+#define PORTNUM		15000
+#define MAXARGS   20
+#define ARGLEN    100
+
+int main(int ac, char *av[])
+{
+	char  *arglist[MAXARGS+1];                    /* ポインタの配列               */ 
+	int   numargs;                                /* 配列の添字                   */
+	char  argbuf[ARGLEN];                         /* ここに入力を読み込む         */
+	struct	sockaddr_in		servadd;								/* 呼び出す番号									*/
+	struct	hostent				*hp;										/* 番号の取得										*/
+	char  *makestring();                          /* メモリの確保などを実行       */
+	int			sock_id, sock_fd;											/* ソケットとfd									*/
+	char		buffer[BUFSIZ];												/* 受信メッセージ								*/
+	int			n_read;																/* メッセージサイズ							*/
+
+	/*if (ac != 3) exit(1);*/
+
+	/* 
+	 * ステップ1：ソケットを作成する
+	 */
+
+	sock_id = socket(AF_INET, SOCK_STREAM, 0);		/* 回線を取得する								*/
+	if ( sock_id == -1)
+		oops("socket");															/* 失敗													*/
+
+	/* 
+	 * ステップ2：サーバに接続する
+	 */
+
+	bzero(&servadd, sizeof(servadd));							/* アドレスを0クリアする				*/
+	hp = gethostbyname(av[1]);										/* ホストのIPアドレスを照合する	*/
+	if (hp == NULL)
+		oops(av[1]);																/* 失敗													*/
+	bcopy(hp->h_addr, (struct sockaddr *)&servadd.sin_addr, hp->h_length);
+	servadd.sin_port = htons(PORTNUM);						/* ポート番号を設定							*/
+	servadd.sin_family = AF_INET;									/* ソケットタイプを設定する			*/
+
+	if (connect(sock_id, (struct sockaddr *)&servadd, sizeof(servadd)) != 0)
+		oops("connect");
+
+	/*
+	 * ステップ3：ディレクトリ名を送り、結果を読み出す
+	 */
+
+	numargs = 0;
+	while (numargs < MAXARGS)
+	{
+		printf("Arg[%d]? ", numargs);
+		if (fgets(argbuf, ARGLEN, stdin) && *argbuf != '\n')
+			arglist[numargs++] = makestring(argbuf);
+		else
+		{
+			if(numargs > 0){
+				arglist[numargs] = NULL;
+				if (write(sock_id, arglist[0], strlen(arglist[0])) == -1)
+					oops("write");
+				if (write(sock_id, "\n", 1) == -1)
+					oops("write");
+				while ((n_read = read(sock_id, buffer, BUFSIZ)) > 0)
+					if (write(1, buffer, n_read) == -1)
+						oops("write");
+				close(sock_id);
+			}
+		}
+	}
+	return 0;
+}
+
+
+char *makestring(char *buf)
+/*
+ * 改行を取り除き、文字列の記憶領域を作る
+ */
+{
+	char  *cp;
+
+	buf[strlen(buf)-1] = '\0';            /* 改行を削除         */
+	cp = malloc(strlen(buf)+1);           /* メモリを確保       */
+	if (cp == NULL){                      /* 失敗したら終了     */
+		fprintf(stderr, "no memory\n");
+		exit(1);
+	}
+	strcpy(cp, buf);                      /* 文字をコピーする   */
+	return cp;                            /* ポインタを返す     */
+}
+    
---
+++ teltelnetd.c
@@ -0,0 +1,114 @@
+/* rlsd.c -	リモートlsサーバ~若干偏執的
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <time.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define PORTNUM		15000												/* リモートlsサーバのポート				*/
+#define HOSTLEN		256
+#define oops(msg)	{ perror(msg); exit(1); }
+
+int main(int ac, char *av[])
+{
+	struct  sockaddr_in   saddr;                /* ここにアドレスを構築する       */
+	struct  hostent       *hp;                  /* アドレスの一部                 */
+	char		hostname[HOSTLEN];									/* 回線ID、ファイルデスクリプタ		*/
+	int     sock_id, sock_fd;                   /* 入出力のためのストリーム       */
+	FILE		*sock_fpi, *sock_fpo;								/* popenを使ってlsを実行する			*/
+	FILE		*pipe_fp;														/* クライアントから送られてくる		*/
+	char		dirname[BUFSIZ];										/* ディレクトリ名									*/
+	char		command[BUFSIZ];										/* popen用												*/
+	int			dirlen, c;													
+
+	/* 
+	 * ステップ1：カーネルにソケットを要求する
+	 */
+ 
+	sock_id = socket(PF_INET, SOCK_STREAM, 0);  /* ソケットを取得する           */
+	if ( sock_id == -1)
+		oops("socket");
+
+	/*
+	 * ステップ2：ソケットにアドレスをバインドする
+	 * 						アドレスはホスト、ポート
+	 */
+
+	bzero((void *)&saddr, sizeof(saddr));     /* 構造体をクリアする             */
+	gethostname(hostname, HOSTLEN);           /* 私はどこ?                      */
+	hp = gethostbyname(hostname);             /* ホスト情報を取得する           */
+  
+	                                          /* ホスト情報を設定               */
+	bcopy((void *)hp->h_addr, (void *)&saddr.sin_addr, hp->h_length);
+	saddr.sin_port = htons(PORTNUM);          /* ソケットポートを設定           */
+	saddr.sin_family = AF_INET;               /* アドレスファミリを設定         */
+	
+	if (bind(sock_id, (struct sockaddr *)&saddr, sizeof(saddr)) != 0)
+		oops("bind");
+
+	/*
+	 * ステップ3：キューサイズを1としてソケットを着信できる状態にする
+	 */
+
+	if (listen(sock_id, 1) != 0)
+		oops("listen");
+
+	/*
+	 * メインループ：accept(),write(),close()
+	 */
+
+	while (1){
+		sock_fd = accept(sock_id, NULL, NULL);  /* 着信を待つ                     */
+		printf("Wow! got a call!\n");
+		if (sock_fd == -1)
+			oops("accept");
+
+
+		/* 読み出し方向をストリームとしてオープン	*/
+		if ((sock_fpi = fdopen(sock_fd, "r")) == NULL)
+			oops("fdopen reading");
+		
+		if (fgets(dirname, BUFSIZ-5, sock_fpi) == NULL)
+			oops("reading dirname");
+/*	sanitize(dirname);*/
+		
+		/* 書き込み方向をストリームとしてオープン	*/
+		if ((sock_fpo = fdopen(sock_fd, "w")) == NULL)
+			oops("popen");
+
+		sprintf(command, "%s\0", dirname);
+		if ((pipe_fp = popen(command, "r")) == NULL)
+			oops("popen");
+			
+		/* lsからソケットにデータを転送	*/
+		while ((c = getc(pipe_fp)) != EOF)
+			putc(c, sock_fpo);	
+		pclose(pipe_fp);
+		fclose(sock_fpo);
+		fclose(sock_fpi);
+	}
+}
+
+int sanitize(char *str)
+/*
+ * 誰かが"; rm *"のようなディレクトリ名を送ってきたときに、
+ * 素直に"ls ; rm *"というコマンドを作るのでは非常にまずい
+ *
+ * そこで、スラッシュと英数字以外の全ての文字を取り除く
+ * もっと良い方法がある。練習問題を参照のこと
+ */
+{
+	char	*src, *dest;
+
+	for (src = dest = str; *src; src++)
+		if (*src == '/' || isalnum(*src))
+			*dest++ = *src;
+	*dest = '\0';
+}
+
